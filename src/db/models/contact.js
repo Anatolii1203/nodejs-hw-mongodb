f13cc0c1868778c6fb4f6e8a907e3d08b921dfde
Anatolii1203 <v120386t@gmail.com>
2024-10-02 19:05:03 +0300

    hw2-mongodb

---
+++ src/db/models/contact.js
@@ -0,0 +1,44 @@
+import { model, Schema } from 'mongoose';
+import { contactTypeList, phoneNumberRegexp } from '../../constants/index.js';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    phoneNumber: {
+      type: String,
+      match: phoneNumberRegexp,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+
+    contactType: {
+      type: String,
+      enum: contactTypeList,
+      required: true,
+      default: 'personal',
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+    },
+  },
+
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactSchema);
---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/env.js';
+import router from './routers/index.js';
+import notFoundMiddleware from './middlewares/notFound.js';
+import errorHandlerMiddleware from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
+
+const PORT = env('PORT', '3000');
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.use(cors());
+  app.use(cookieParser());
+
+  app.use(router);
+
+  app.use(notFoundMiddleware);
+
+  app.use(errorHandlerMiddleware);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
