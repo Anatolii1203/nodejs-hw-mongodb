bd554413668d27bea77b841f27434a571c8c0210
Anatolii1203 <v120386t@gmail.com>
2024-10-31 15:34:37 +0300

    hw5-auth

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { validateBody } from '../middlewares/validateBody.js';
-import { loginUserSchema, registerUserSchema } from '../validation /auth.js';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   loginUserController,
